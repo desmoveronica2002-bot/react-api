ade3166898de483c1a867544eb29d72dad9c7af0
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-03-17 18:02:04 +0100

    milleston 2 finish

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ const CastList = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data); 
+        setCast(data); 
       })
   }, []);
 
   return (
-    <div>
-      <h1>Attrici </h1>
+    <div className="container">
+      {cast.map((actor) => (
+        <div key={actor.id} className="cast-card">
+          <img src={actor.image} alt={actor.name} className="cast-image" />
+          <h3>{actor.name}</h3>
+          <p><strong>Anno di nascita:</strong> {actor.birth_year}</p>
+          <p><strong>Nazionalità:</strong> {actor.nationality}</p>
+          <p>{actor.biography}</p>
+          <p><strong>Riconoscimenti:</strong> {actor.awards}</p>
+        </div>
+      ))}
     </div>
   );
 };
